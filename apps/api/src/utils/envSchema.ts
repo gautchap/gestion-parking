4607ee2df5c1665d83b0284fcf9dc093876b292f
gautchap <gautchap+github@outlook.com>
2024-08-31 18:17:33 +0200

    api:feat: create schema for environment variables

---
+++ apps/api/src/utils/envSchema.ts
@@ -0,0 +1,21 @@
+import { z } from '@repo/schemas';
+
+export const envSchemaServer = z.object({
+  DATABASE_URL: z.string().url(),
+  PORT: z.number().default(3001),
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+});
+
+const envServer = envSchemaServer.safeParse({
+  DATABASE_URL: process.env.DATABASE_URL,
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+});
+
+if (!envServer.success) {
+  throw new Error('There is an error with the server environment variables');
+}
+
+export type EnvSchemaServerType = z.infer<typeof envSchemaServer>;
