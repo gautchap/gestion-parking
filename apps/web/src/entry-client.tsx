0393589e7eb63b67082adb27c31d6a3e76f2536f
gautchap <gautchap+github@outlook.com>
2024-08-31 11:42:37 +0200

    web:feat: add react-router-dom

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,11 +1,13 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    document.querySelector("#root") as HTMLElement,
+    <React.StrictMode>
+        <BrowserRouter>
+            <div>Hello world</div>
+        </BrowserRouter>
+    </React.StrictMode>
+);
--- apps/web/src/entry-server.tsx
+++ apps/web/src/entry-server.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import App from './App'
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
 
-export function render() {
-  const html = ReactDOMServer.renderToString(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-  return { html }
+export function render(url: string) {
+    return renderToString(
+        <React.StrictMode>
+            <StaticRouter location={url}>
+                <div>Hello world</div>
+            </StaticRouter>
+        </React.StrictMode>
+    );
 }
