acce4db5f4058158deb460b86dfb42607c5f142a
gautchap <gautchap+github@outlook.com>
2024-08-31 19:10:44 +0200

    api:refactor: add remove method to place controller

--- apps/api/src/place/place.controller.spec.ts
+++ apps/api/src/place/place.controller.spec.ts
@@ -3,7 +3,8 @@ import { PlaceController } from './place.controller';
 import { PlaceService } from './place.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { randomUUID } from 'node:crypto';
-import { ConflictException } from '@nestjs/common';
+import { BadRequestException, ConflictException } from '@nestjs/common';
+import { ticketSchema } from '@repo/schemas';
 
 describe('PlaceController', () => {
   let controller: PlaceController;
@@ -41,8 +42,58 @@ describe('PlaceController', () => {
 
       const result = controller.getTicket();
 
-      expect(result).rejects.toThrow(
+      await expect(result).rejects.toThrow(
         new ConflictException('No available place'),
+      );
+    });
+  });
+  describe('removeTicket', () => {
+    it('should remove the ticket and return a success message', async () => {
+      const ticketId = randomUUID().toString();
+      const placeId = 1;
+      const place = {
+        id: placeId,
+        free: true,
+        ticket: null,
+        parkingId: randomUUID().toString(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const removeTicketDto = { ticketId, placeId };
+
+      const expectedResult = { message: 'Ticket removed' };
+
+      jest.spyOn(service, 'RemoveTicket').mockResolvedValue(place);
+
+      const result = await controller.removeTicket(removeTicketDto);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should throw a BadRequestException if ticket or place not found', async () => {
+      const ticketId = randomUUID().toString();
+      const placeId = 1;
+      const removeTicketDto = { ticketId, placeId };
+
+      jest.spyOn(service, 'RemoveTicket').mockResolvedValue(null);
+
+      await expect(controller.removeTicket(removeTicketDto)).rejects.toThrow(
+        new BadRequestException('Ticket or Place not found'),
+      );
+    });
+
+    it('should throw a BadRequestException if ticket or place not matching dto', async () => {
+      const ticketId = 9999;
+      const placeId = randomUUID().toString();
+      const removeTicketDto = { ticketId, placeId } as any;
+
+      const removeTicketParsed = ticketSchema.safeParse(removeTicketDto);
+
+      expect(removeTicketParsed.success).toBe(false);
+
+      await expect(controller.removeTicket(removeTicketDto)).rejects.toThrow(
+        BadRequestException,
       );
     });
   });
--- apps/api/src/place/place.controller.ts
+++ apps/api/src/place/place.controller.ts
@@ -1,5 +1,15 @@
-import { ConflictException, Controller, Get } from '@nestjs/common';
+import { ticketSchema } from '@repo/schemas';
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  Get,
+  Put,
+  UsePipes,
+} from '@nestjs/common';
 import { PlaceService } from './place.service';
+import { ZodValidationPipe } from '../utils/validation.pipes';
 
 @Controller('place')
 export class PlaceController {
@@ -12,5 +22,15 @@ export class PlaceController {
     if (!place) throw new ConflictException('No available place');
 
     return { ticketId: place.ticket, placeId: place.id };
+  }
+
+  @Put('removeTicket')
+  @UsePipes(new ZodValidationPipe(ticketSchema))
+  async removeTicket(@Body() body: { ticketId: string; placeId: number }) {
+    const place = await this.placeService.RemoveTicket(body);
+
+    if (!place) throw new BadRequestException('Ticket or Place not found');
+
+    return { message: 'Ticket removed' };
   }
 }
