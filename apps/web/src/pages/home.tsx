6a2939372c00e622b7c11cdcf1eef7b7c2267323
gautchap <gautchap+github@outlook.com>
2024-08-31 18:34:42 +0200

    web:refactor: update home page with getTicket method

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -1,3 +1,18 @@
+import { addTicket } from "../reducer/ticketSlice";
+import { useAppDispatch } from "../hooks/redux-hooks";
+import { getTicket } from "../services/ticket.service";
+
 export default function Home() {
-    return <div>Home</div>;
+    const dispatch = useAppDispatch();
+
+    const getPlace = async () => {
+        const place = await getTicket();
+        return dispatch(addTicket(place));
+    };
+
+    return (
+        <>
+            <button onClick={getPlace}>Get Ticket</button>
+        </>
+    );
 }
