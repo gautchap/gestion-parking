15f39faf2aad4f48dd0ee9f5430efe46fcb04eec
gautchap <gautchap+github@outlook.com>
2024-08-31 19:10:27 +0200

    api:feat: create zod validation pipe for dto

---
+++ apps/api/src/utils/validation.pipes.ts
@@ -0,0 +1,15 @@
+import type { ZodSchema } from '@repo/schemas';
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+}
