01c92b09159f1d47d5784d30827377874c1e25a3
gautchap <gautchap+github@outlook.com>
2024-09-01 13:16:33 +0200

    web:refactor: dispatch parking action when fetching parking data

--- apps/web/src/pages/parking.tsx
+++ apps/web/src/pages/parking.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { getParking } from "../services/parking.service";
+import { useAppDispatch } from "../hooks/redux-hooks";
+import { addParking } from "../reducer/parkingSlice";
 
 export default function Parking() {
+    const dispatch = useAppDispatch();
     useEffect(() => {
         const controller = new AbortController();
         (async () => {
             const _parking = await getParking(controller.signal);
-            console.log(_parking);
+            dispatch(addParking(_parking));
         })();
 
         return () => controller.abort();
