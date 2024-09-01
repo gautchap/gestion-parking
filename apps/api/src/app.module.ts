27cce2c75d8498eefacde0cb4afa27f06df58fb7
gautchap <gautchap+github@outlook.com>
2024-09-01 17:40:37 +0200

    api:refactor: use nest serve static to render vite app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,9 +8,18 @@ import { PlaceController } from './place/place.controller';
 import { PlaceService } from './place/place.service';
 import { ParkingService } from './parking/parking.service';
 import { ParkingController } from './parking/parking.controller';
+import { join } from 'node:path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'web', 'dist'),
+      exclude: ['/api*'],
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController, PlaceController, ParkingController],
   providers: [AppService, PrismaService, PlaceService, ParkingService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-
-  const port = process.env.PORT || 3001;
+  app.setGlobalPrefix('api');
+  const port = process.env.PORT || 3000;
 
   await app.listen(port, '0.0.0.0');
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
