851a6569281de29e68d5f7e4a8916b5e9b9d5fe5
gautchap <gautchap+github@outlook.com>
2024-09-01 20:54:12 +0200

    web:refactor: change style on path

--- apps/web/src/components/nav-bar.tsx
+++ apps/web/src/components/nav-bar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 type NavBarProps = {
     routes: {
@@ -8,13 +8,15 @@ type NavBarProps = {
 };
 
 export default function NavBar({ routes }: NavBarProps) {
+    const location = useLocation();
+
     return (
-        <nav className="h-12 bg-orange-400">
+        <nav className="mx-auto my-1 h-12 max-w-xs rounded-full bg-blue-300/50">
             <ul className="flex h-full items-center justify-evenly">
                 {routes.map(({ name, path }) => {
                     return (
-                        <li key={path} className="inline-block">
-                            <Link to={path}>{name}</Link>
+                        <li key={path} className={`${location.pathname === path ? "text-slate-500" : ""} inline-block`}>
+                            <Link to={path}>{name.charAt(0).toUpperCase() + name.slice(1)}</Link>
                         </li>
                     );
                 })}
