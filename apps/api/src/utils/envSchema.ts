41953045c270c9ff659d14192213500dd7eef574
gautchap <gautchap+github@outlook.com>
2024-09-01 14:02:54 +0200

    api:chore: add directUrl to prisma schema for migrations

--- apps/api/src/utils/envSchema.ts
+++ apps/api/src/utils/envSchema.ts
@@ -2,6 +2,7 @@ import { z } from '@repo/schemas';
 
 export const envSchemaServer = z.object({
   DATABASE_URL: z.string().url(),
+  DIRECT_URL: z.string().url(),
   PORT: z.number().default(3001),
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
@@ -10,6 +11,7 @@ export const envSchemaServer = z.object({
 
 const envServer = envSchemaServer.safeParse({
   DATABASE_URL: process.env.DATABASE_URL,
+  DIRECT_URL: process.env.DIRECT_URL,
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
 });
