7fcf595ee41a6ac18b0273caffe738ce0179abc8
gautchap <gautchap+github@outlook.com>
2024-09-02 15:06:42 +0200

    web:refactor: remove unused var

--- apps/web/src/services/fetcher.ts
+++ apps/web/src/services/fetcher.ts
@@ -1,8 +1,6 @@
 /* global RequestInit */
 import { z } from "@repo/schemas";
 
-export const apiUrl = import.meta.env.VITE_API_URL;
-
 interface RequestConfig<T> {
     data?: unknown;
     zodSchema?: z.ZodSchema<T>;
